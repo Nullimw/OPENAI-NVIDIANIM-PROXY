5da96575a3ce16d38ad738c70255bcbd8943befb
Nullimw <161916278+Nullimw@users.noreply.github.com>
2025-10-03 11:39:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -87,6 +87,7 @@ if (!nimModel) {
       messages: messages,
       temperature: temperature || 0.6,
       max_tokens: max_tokens ||9024,
+      chat_template_kwargs: {"thinking":true},
       stream: stream || true
     };
     
